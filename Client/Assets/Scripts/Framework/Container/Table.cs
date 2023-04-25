ee3ea97cf56e07b4d4a87f3b6c6babd4b59d55e8
KIPKIPS <2432719656@qq.com>
2023-04-25 18:11:14 +0800

    界面参数传递

---
+++ Client/Assets/Scripts/Framework/Container/Table.cs
@@ -0,0 +1,40 @@
+﻿// author:KIPKIPS
+// date:2023.04.25 16:25
+// describe:table容器
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Framework.Container {
+    public class Table {
+        private dynamic _strTable;
+        private Dictionary<int, dynamic> _intTable;
+        private dynamic StrTable => _strTable ??= new ExpandoObject();
+        private Dictionary<int, dynamic> IntTable => _intTable ??= new Dictionary<int, dynamic>();
+        public dynamic this[string key] {
+            get {
+                return null;
+                // return data != null ? data : null;
+            }
+            set {
+                if (StrTable.ContainsKey(key)) {
+                    StrTable[key] = value;
+                } else {
+                    StrTable.Add(key,value);
+                }
+            }
+        }
+        public dynamic this[int index] {
+            get {
+                IntTable.TryGetValue(index, out var data);
+                return data != null ? data : null;
+            }
+            set {
+                if (IntTable.ContainsKey(index)) {
+                    IntTable[index] = value;
+                } else {
+                    IntTable.Add(index,value);
+                }
+            }
+        }
+    }
+}
