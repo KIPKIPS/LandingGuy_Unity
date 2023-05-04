f6c5e7671c2c8eac01982bbb0ca0ca0d8be1aef3
KIPKIPS <2432719656@qq.com>
2023-05-04 18:53:53 +0800

    处理bindable类型

--- Client/Assets/Scripts/Framework/UI/Binding/Runtime/Base/Bindable.cs
+++ Client/Assets/Scripts/Framework/UI/Binding/Runtime/Base/Bindable.cs
@@ -2,7 +2,6 @@
 // date:2023.04.17 23:27
 // describe:可绑定处理
 using System;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -33,50 +32,55 @@ namespace Framework.UI {
             _key = key;
             _uiBinding = uiBinding;
         }
-        // public Bindable(UIBinding uiBinding,string key,UnityAction unityAction) {
-        //     if (!uiBinding.BinderDataDict.TryGetValue(key, out var data)) return;
-        //     var baseBinder = UIBinding.GetBaseBinder(UIBinding.GetType(data.bindObj));
-        //     baseBinder.SetAction(data.bindObj, data.bindFieldId,unityAction );
-        // }
-        // public Bindable(UIBinding uiBinding,string key,UnityAction<T> unityAction) {
-        //     if (!uiBinding.BinderDataDict.TryGetValue(key, out var data)) return;
-        //     var baseBinder = UIBinding.GetBaseBinder(UIBinding.GetType(data.bindObj));
-        //     switch (typeof(T).Name) {
-        //         case "Vector2":
-        //             if (unityAction is UnityAction<Vector2> v2) baseBinder.SetActionVector2(data.bindObj, data.bindFieldId,v2 );
-        //             break;
-        //         default:
-        //             LUtil.LogWarning("Failure Binding",$"Unregistered binding type : UnityAction<{typeof(T).Name}>");
-        //             break;
-        //     }
-        // }
         private void UpdateBind(dynamic value) {
             if (!_uiBinding.BinderDataDict.TryGetValue(_key, out var data)) return;
             var baseBinder = UIBinding.GetBaseBinder(UIBinding.GetType(data.bindObj));
             switch ((LinkerType)data.fieldType) {
-                case LinkerType.String:
-                    if (value is string stringValue) baseBinder.SetString(data.bindObj, data.bindFieldId, stringValue);
-                    break;
-                case LinkerType.Int32:
-                    if (value is int intValue) baseBinder.SetInt32(data.bindObj, data.bindFieldId,intValue);
-                    break;
-                case LinkerType.Boolean:
-                    if (value is bool boolValue) baseBinder.SetBoolean(data.bindObj, data.bindFieldId,boolValue);
-                    break;
-                case LinkerType.Color:
-                    if (value is Color colorValue) baseBinder.SetColor(data.bindObj, data.bindFieldId,colorValue);
-                    break;
                 case LinkerType.Vector2:
                     if (value is Vector2 v2Value) baseBinder.SetVector2(data.bindObj, data.bindFieldId,v2Value);
                     break;
                 case LinkerType.Vector3:
                     if (value is Vector3 v3Value) baseBinder.SetVector3(data.bindObj, data.bindFieldId,v3Value);
                     break;
+                case LinkerType.Quaternion:
+                    if (value is Quaternion quaternionValue) baseBinder.SetQuaternion(data.bindObj, data.bindFieldId,quaternionValue);
+                    break;
+                case LinkerType.Boolean:
+                    if (value is bool boolValue) baseBinder.SetBoolean(data.bindObj, data.bindFieldId,boolValue);
+                    break;
+                case LinkerType.Int32:
+                    if (value is int intValue) baseBinder.SetInt32(data.bindObj, data.bindFieldId,intValue);
+                    break;
+                case LinkerType.String:
+                    if (value is string stringValue) baseBinder.SetString(data.bindObj, data.bindFieldId, stringValue);
+                    break;
+                case LinkerType.Single:
+                    if (value is float floatValue) baseBinder.SetSingle(data.bindObj, data.bindFieldId, floatValue);
+                    break;
+                case LinkerType.Color:
+                    if (value is Color colorValue) baseBinder.SetColor(data.bindObj, data.bindFieldId,colorValue);
+                    break;
+                case LinkerType.Sprite:
+                    if (value is Sprite spriteValue) baseBinder.SetSprite(data.bindObj, data.bindFieldId, spriteValue);
+                    break;
+                case LinkerType.Char:
+                    if (value is char charValue) baseBinder.SetChar(data.bindObj, data.bindFieldId, charValue);
+                    break;
+                case LinkerType.Rect:
+                    if (value is Rect rectValue) baseBinder.SetRect(data.bindObj, data.bindFieldId, rectValue);
+                    break;
+                case LinkerType.Object:
+                    if (value is object objValue) baseBinder.SetSystemObject(data.bindObj, data.bindFieldId, objValue);
+                    break;
                 case LinkerType.UnityAction:
                     if (value is UnityAction action) baseBinder.SetAction(data.bindObj, data.bindFieldId,action);
                     break;
                 case LinkerType.UnityActionVector2:
                     if (value is UnityAction<Vector2> actionVector2) baseBinder.SetActionVector2(data.bindObj, data.bindFieldId,actionVector2);
+                    break;
+                case LinkerType.RectTransform:
+                    break;
+                case LinkerType.UIBinding:
                     break;
                 default:
                     LUtil.LogWarning("Failure Binding",$"Unregistered binding type : {((LinkerType)data.fieldType).ToString()}");
